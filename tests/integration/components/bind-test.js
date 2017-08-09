f84bd0a3acd2fd9872189dc8bf665f17d3cf6d1a
Sergio Arbeo <serabe@gmail.com>
2017-08-10 01:28:42 +0200

    Simplify tests

--- tests/integration/components/bind-test.js
+++ tests/integration/components/bind-test.js
@@ -2,124 +2,141 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 
-const { run, set } = Ember;
+const { get, run, set } = Ember;
 
 moduleForComponent('helper:bind', 'Integration | Helper | bind', {
   integration: true,
 
   beforeEach() {
-    this.set('model', Ember.Object.create({
-      prop: 'object',
-      methodName: 'method',
+    let testContext = this;
+    function recordCall(...args) {
+      set(testContext, 'context', this);
+      set(testContext, 'arguments', args);
+    }
 
-      method(value) {
-        set(this, 'prop', value);
-      },
+    let actions = { method: recordCall };
+    let model = { method: recordCall, actions };
 
-      actions: {
-        prop: 'actions',
-        method(value) {
-          set(this, 'prop', value);
-        }
+    this.set('model', model);
+    this.set('method', recordCall);
+
+    this.callAction = function() {
+      run(() => this.$('button').click());
+    }
+
+    this.assertContext = function(assert, expectedContext, msg) {
+      let actualContext = get(testContext, 'context');
+      if (!msg) {
+        msg = `Expected context ${expectedContext} but got ${actualContext}`;
       }
-    }));
-
-    this.set('prop', 'root');
-    this.set('method', function(value) { set(this, 'prop', value)});
-
-    this.callAction = function(value) {
-      run(() => this.$('input').val(value).trigger('input'));
-    }
-
-    this.assertNewValueWasSetInRoot = function(assert, newValue) {
-      let model = this.get('model');
-      assert.equal(this.get('prop'), newValue, 'newValue was set in root');
-      assert.equal(model.get('prop'), 'object', 'newValue was not set in model');
-      assert.equal(model.get('actions.prop'), 'actions', 'newValue was not set in actions hash');
-    }
-    this.assertNewValueWasSetInModel = function(assert, newValue) {
-      let model = this.get('model');
-      assert.equal(this.get('prop'), 'root', 'newValue was not set in root');
-      assert.equal(model.get('prop'), newValue, 'newValue was set in model');
-      assert.equal(model.get('actions.prop'), 'actions', 'newValue was not set in actions hash');
+      assert.strictEqual(actualContext, expectedContext);
     };
-    this.assertNewValueWasSetInActions = function(assert, newValue) {
-      let model = this.get('model');
-      assert.equal(this.get('prop'), 'root', 'newValue was not set in root');
-      assert.equal(model.get('prop'), 'object', 'newValue was not set in model');
-      assert.equal(model.get('actions.prop'), newValue, 'newValue was set in actions hash');
-    };
+
+    this.assertArguments = function(assert, expectedArguments, msg) {
+      let actualArguments = get(testContext, 'arguments');
+      if (!msg) {
+        msg = `Expected arguments ${expectedArguments} but got ${actualArguments}`;
+      }
+      assert.deepEqual(actualArguments, expectedArguments);
+    }
   }
 });
 
 test('it changes the context of a function, function: method, context: implicit this', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind method) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind method)}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInRoot(assert, 'hola');
+  this.assertContext(assert, this);
+  this.assertArguments(assert, []);
 });
 
 test('it changes the context of a function, function: method, context: this', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind method target=this) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind method target=this) value="target.value"}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInRoot(assert, 'hola');
+  this.assertContext(assert, this);
+  this.assertArguments(assert, []);
 });
 
 test('it changes the context of a function, function: method, context: model.actions', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind method target=model.actions) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind method target=model.actions)}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInActions(assert, 'hola');
+  this.assertContext(assert, get(this, 'model.actions'));
+  this.assertArguments(assert, []);
 });
 
 test('it changes the context of a function, function: model.method, context: implicit model', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind model.method) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind model.method)}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInModel(assert, 'hola');
+  this.assertContext(assert, get(this, 'model'));
+  this.assertArguments(assert, []);
 });
 
 test('it changes the context of a function, function: model.method, context: model', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind model.method target=model) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind model.method target=model)}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInModel(assert, 'hola');
+  this.assertContext(assert, get(this, 'model'));
+  this.assertArguments(assert, []);
 });
 
 test('it changes the context of a function, function: model.method, context: model.actions.', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind model.method target=model.actions) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind model.method target=model.actions)}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInActions(assert, 'hola');
+  this.assertContext(assert, get(this, 'model.actions'));
+  this.assertArguments(assert, []);
 });
 
 test('it changes the context of a function, function: model.actions.method, context: implicit model.actions.', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind model.actions.method) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind model.actions.method)}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInActions(assert, 'hola');
+  this.assertContext(assert, get(this, 'model.actions'));
+  this.assertArguments(assert, []);
 });
 
 test('it changes the context of a function, function: model.actions.method, context: model.actions.', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind model.actions.method target=model.actions) value="target.value")}}`);
+  this.render(hbs`<button {{action (bind model.actions.method target=model.actions)}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInActions(assert, 'hola');
+  this.assertContext(assert, get(this, 'model.actions'));
+  this.assertArguments(assert, []);
 });
 
 test('it passes the extra argument to bind', function(assert) {
-  this.render(hbs`{{input value="hello" input=(action (bind model.actions.method "adios") value="target.value")}}`);
+  this.render(hbs`<button {{action (bind model.actions.method "adios")}}>Click</button>`);
 
-  this.callAction('hola');
+  this.callAction();
 
-  this.assertNewValueWasSetInActions(assert, 'adios');
-})
+  this.assertContext(assert, get(this, 'model.actions'));
+  this.assertArguments(assert, ['adios']);
+});
+
+test('it passes the extra argument to bind and keeps explicit target', function(assert) {
+  this.render(hbs`<button {{action (bind model.actions.method "adios" target=model)}}>Click</button>`);
+
+  this.callAction();
+
+  this.assertContext(assert, get(this, 'model'));
+  this.assertArguments(assert, ['adios']);
+});
+
+test('it passes the extra arguments to bind', function(assert) {
+  this.render(hbs`<button {{action (bind model.actions.method "adios" 1 model)}}>Click</button>`);
+
+  this.callAction();
+
+  this.assertContext(assert, get(this, 'model.actions'));
+  this.assertArguments(assert, ['adios', 1, get(this, 'model')]);
+});
