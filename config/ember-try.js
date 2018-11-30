115d5bf1c49ea9d13eb3ddd6e0157e2499109fcb
Sergio Arbeo <serabe@gmail.com>
2018-11-30 11:35:12 +0100

    Support named arguments
    Closes #6

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,28 +8,43 @@ module.exports = function() {
     getChannelURL('canary')
   ]).then((urls) => {
     return {
+      useYarn: true,
       scenarios: [
         {
           name: 'ember-lts-2.16',
+          command: 'ember test --filter "Helper | bind"',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
           },
           npm: {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.16.0'
+              'ember-source': '~2.16.0',
+              'ember-angle-bracket-invocation-polyfill': '1.2.3',
+              'ember-named-arguments-polyfill': '1.0.0'
             }
           }
         },
         {
           name: 'ember-lts-2.18',
+          command: 'ember test --filter "Helper | bind"',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
           },
           npm: {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
+              'ember-source': '~2.18.0',
+              'ember-angle-bracket-invocation-polyfill': '1.2.3',
+              'ember-named-arguments-polyfill': '1.0.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0'
             }
           }
         },
@@ -59,12 +74,17 @@ module.exports = function() {
         },
         {
           name: 'ember-default',
+          command: 'ember test --filter "Helper | bind"',
           npm: {
-            devDependencies: {}
+            devDependencies: {
+              'ember-angle-bracket-invocation-polyfill': '1.2.3',
+              'ember-named-arguments-polyfill': '1.0.0'
+            }
           }
         },
         {
           name: 'ember-default-with-jquery',
+          command: 'ember test --filter "Helper | bind"',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
               'jquery-integration': true
@@ -72,7 +92,9 @@ module.exports = function() {
           },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1'
+              '@ember/jquery': '^0.5.1',
+              'ember-angle-bracket-invocation-polyfill': '1.2.3',
+              'ember-named-arguments-polyfill': '1.0.0'
             }
           }
         }
