dab0de9edf5e2170b810a7cb5a20f8155e7953cd
Sergio Arbeo <serabe@gmail.com>
2019-03-08 12:52:04 +0100

    Try to fix build in a PR

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,51 @@
+/* eslint-env node */
 module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ["ember"],
+  extends: ["eslint:recommended", "plugin:ember/recommended"],
   env: {
     browser: true
   },
-  rules: {
-  },
+  rules: {},
   overrides: [
     // node files
     {
       files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js'
+        ".eslintrc.js",
+        ".template-lintrc.js",
+        "ember-cli-build.js",
+        "index.js",
+        "testem.js",
+        "blueprints/*/index.js",
+        "config/**/*.js",
+        "tests/dummy/config/**/*.js"
       ],
       excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
+        "addon/**",
+        "addon-test-support/**",
+        "app/**",
+        "tests/dummy/app/**"
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
         ecmaVersion: 2015
       },
       env: {
         browser: false,
         node: true
       },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      })
+      plugins: ["node"],
+      rules: Object.assign(
+        {},
+        require("eslint-plugin-node").configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+        }
+      )
     }
   ]
 };
--- config/ember-try.js
+++ config/ember-try.js
@@ -1,91 +1,96 @@
-const getChannelURL = require('ember-source-channel-url');
+/* eslint-env node */
+const getChannelURL = require("ember-source-channel-url");
 
-module.exports = function () {
+module.exports = function() {
   return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary')
-  ]).then((urls) => {
+    getChannelURL("release"),
+    getChannelURL("beta"),
+    getChannelURL("canary")
+  ]).then(urls => {
     return {
+      command: "ember test --reporter xunit",
+      npmOptions: ["--loglevel=silent", "--no-shrinkwrap=true"],
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          command: 'ember test --filter "Helper | bind"',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0',
-              'ember-angle-bracket-invocation-polyfill': '1.2.3',
-              'ember-named-arguments-polyfill': '1.0.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.4',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.4.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.8',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.8.0'
-            }
-          }
-        },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[0]
-            }
-          }
-        },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1]
-            }
-          }
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2]
-            }
-          }
-        },
-        {
-          name: 'ember-default',
-          command: 'ember test --filter "Helper | bind"',
-          npm: {
-            devDependencies: {
-              'ember-angle-bracket-invocation-polyfill': '1.2.3',
-              'ember-named-arguments-polyfill': '1.0.0'
-            }
-          }
-        },
-        {
-          name: 'ember-default-with-jquery',
+          name: "ember-lts-2.18",
           command: 'ember test --filter "Helper | bind"',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true
+              "jquery-integration": true
             })
           },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-angle-bracket-invocation-polyfill': '1.2.3',
-              'ember-named-arguments-polyfill': '1.0.0'
+              "@ember/jquery": "^0.5.1",
+              "ember-source": "~2.18.0",
+              "ember-angle-bracket-invocation-polyfill": "1.2.3",
+              "ember-named-arguments-polyfill": "1.0.0"
+            }
+          }
+        },
+        {
+          name: "ember-lts-3.4",
+          npm: {
+            devDependencies: {
+              "ember-source": "~3.4.0"
+            }
+          }
+        },
+        {
+          name: "ember-lts-3.8",
+          npm: {
+            devDependencies: {
+              "ember-source": "~3.8.0"
+            }
+          }
+        },
+        {
+          name: "ember-release",
+          npm: {
+            devDependencies: {
+              "ember-source": urls[0]
+            }
+          }
+        },
+        {
+          name: "ember-beta",
+          npm: {
+            devDependencies: {
+              "ember-source": urls[1]
+            }
+          }
+        },
+        {
+          name: "ember-canary",
+          npm: {
+            devDependencies: {
+              "ember-source": urls[2]
+            }
+          }
+        },
+        {
+          name: "ember-default",
+          command: 'ember test --filter "Helper | bind"',
+          npm: {
+            devDependencies: {
+              "ember-angle-bracket-invocation-polyfill": "1.2.3",
+              "ember-named-arguments-polyfill": "1.0.0"
+            }
+          }
+        },
+        {
+          name: "ember-default-with-jquery",
+          command: 'ember test --filter "Helper | bind"',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              "jquery-integration": true
+            })
+          },
+          npm: {
+            devDependencies: {
+              "@ember/jquery": "^0.5.1",
+              "ember-angle-bracket-invocation-polyfill": "1.2.3",
+              "ember-named-arguments-polyfill": "1.0.0"
             }
           }
         }
